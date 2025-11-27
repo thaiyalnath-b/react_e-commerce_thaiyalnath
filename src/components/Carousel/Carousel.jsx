ec38e53c0a24eea2e15a9cfe6503ccd5dab2bda7
ThaiyalnathB <thaiyalnath2005@gmail.com>
2025-11-27 17:52:17 +0530

    styles

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -8,13 +8,13 @@ const Carousel = () => {
           <img src="/images/Carousel/fin1.webp" className="d-block w-100" alt="..." />
         </div>
         <div className="carousel-item">
-          <img src="/images/Carousel/fin2.avif" className="d-block w-100" alt="..." />
+          <img src="/images/Carousel/fin4.avif" className="d-block w-100" alt="..." />
         </div>
         <div className="carousel-item">
           <img src="/images/Carousel/fin3.avif" className="d-block w-100" alt="..." />
         </div>
         <div className="carousel-item">
-          <img src="/images/Carousel/fin4.avif" className="d-block w-100" alt="..." />
+          <img src="/images/Carousel/fin2.avif" className="d-block w-100" alt="..." />
         </div>
       </div>
       <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
